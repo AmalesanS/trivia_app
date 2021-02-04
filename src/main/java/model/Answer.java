db3743fb84cc1aac35a3495dcac9e1a2424c7801
jenndalt <jennifer.dalton@cognizant.com>
2021-02-03 18:15:06 -0600

    WIP - passing to Amelsan - setting up answer and question models

--- src/main/java/model/Answer.java
+++ src/main/java/model/Answer.java
@@ -4,12 +4,26 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
-@AllArgsConstructor
+@Entity
+@Table(name = "answer")
 public class Answer {
+    @Id
+    private Integer id;
+    @ManyToOne
+    @Column(name = "question")
     private Question question;
     private char choice;
     private String answer;
     private boolean correct;
+
+    public Answer(Question question, char choice, String answer, boolean correct) {
+        this.question = question;
+        this.choice = choice;
+        this.answer = answer;
+        this.correct = correct;
+    }
 }
--- src/main/java/model/Question.java
+++ src/main/java/model/Question.java
@@ -3,16 +3,24 @@ package model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "question")
 public class Question {
-    Integer id;
+    @Id
+    private Integer id;
     String question;
     String timestamp;
+    @OneToMany
     List<Answer> answers;
 
     public Question(Integer id, String question, String timestamp) {
